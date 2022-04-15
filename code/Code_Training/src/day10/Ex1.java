a5099355189efe0e73eeac78a1683270ee309909
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-04-15 21:58:51 +0900

    day 10

---
+++ code/Code_Training/src/day10/Ex1.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class Ex1 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    int a = 10;  
+    int b = 0; 
+    /* 
+    if (b == 0){ 
+        System.out.println("0으로 나눌수 없습니다."); 
+        System.exit(0); 
+    } 
+     */         
+
+    System.out.println(" a = " + a + " b = " + b ); 
+    System.out.println(" a/b = " + (a/b) ); 
+    System.out.println("나눗셈이 수행되었습니다."); 
+  }
+
+}
---
+++ code/Code_Training/src/day10/Ex10_10.java
@@ -0,0 +1,20 @@
+package day10;
+
+import java.io.FileInputStream;
+ 
+public class Ex10_10 {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = new FileInputStream("c:/users/nambi/OneDrive/바탕 화면/TIL/file/data1.txt");
+        int ch;
+        byte[] bt = new byte[1024];
+ 
+        int i = 0;
+        while ((ch = fis.read()) != -1) {
+            bt[i] = (byte) ch;
+            i++;
+        }
+ 
+        System.out.print(new String(bt));
+        fis.close();
+    }
+}
