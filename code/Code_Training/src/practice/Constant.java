7ad2765f9ed5968ce22dceb3d3c9e4661fbf9750
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-05-04 09:17:34 +0900

    practice

--- code/Code_Training/src/practice/Constant.java
+++ code/Code_Training/src/practice/Constant.java
@@ -1,9 +1,9 @@
 package practice;
 
 public class Constant {
-  public static final String url = "";
+  public static final String url = "jdbc:mysql://127.0.0.1:3306/javadb?useUnicode=true&characterEncoding=utf8";
   public static final String user = "javauser";
   public static final String password = "1234";
-  public static final String driver = "";
+  public static final String driver = "com.mysql.cj.jdbc.Driver";
       
 }
--- code/Code_Training/src/practice/practiceDAO.java
+++ code/Code_Training/src/practice/practiceDAO.java
@@ -4,11 +4,43 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class practiceDAO {
   
-  public void readAll() {
+  public List<practiceDTO> readAll() {
+    List<practiceDTO> list = new ArrayList<>();
+    Connection con = DBOpen.getConnection();
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
     
+    StringBuffer sql = new StringBuffer();
+    sql.append(" select * from memo ");
+    
+    try {
+      pstmt = con.prepareStatement(sql.toString());
+      rs = pstmt.executeQuery();
+      
+      while(rs.next()) {
+        practiceDTO dto = new practiceDTO();
+        dto.setMemono(rs.getInt("memono"));
+        dto.setWname(rs.getString("wname"));
+        dto.setTitle(rs.getString("title"));
+        dto.setContent(rs.getString("content"));
+        dto.setPasswd(rs.getString("passwd"));
+        dto.setDate(rs.getDate("wdate"));
+        
+        list.add(dto);
+      }
+      
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      DBClose.close(con, pstmt, rs);
+    }
+    return list;
   }
   
   public void update() {
@@ -20,6 +52,20 @@ public class practiceDAO {
   }
 
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   public practiceDTO readOne(int num) {
     // TODO Auto-generated method stub
     practiceDTO dto = null;
@@ -28,20 +74,22 @@ public class practiceDAO {
     ResultSet rs = null;
     
     StringBuffer sql = new StringBuffer();
-    sql.append("");
+    sql.append(" select wname, title, content, wdate ");
+    sql.append(" from memo ");
+    sql.append(" where memono = ? ");
     
     try {
       pstmt = con.prepareStatement(sql.toString());
+      pstmt.setInt(1, num);
       rs = pstmt.executeQuery();
       
       if(rs.next()) {
         dto = new practiceDTO();
-        dto.setMemono(rs.getInt("memono"));
+        dto.setMemono(num);
         dto.setWname(rs.getString("wname"));
         dto.setTitle(rs.getString("title"));
         dto.setContent(rs.getString("content"));
-        dto.setPasswd(rs.getString("passwd"));
-        dto.setDate(rs.getDate("date"));
+        dto.setDate(rs.getDate("wdate"));
       }
       
     } catch (SQLException e) {
