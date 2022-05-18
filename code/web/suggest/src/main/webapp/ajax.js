3c0935a68b0c2b9943a1dff647c94776041df8f4
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-05-18 11:45:34 +0900

    TIL

--- code/web/suggest/src/main/webapp/ajax.js
+++ code/web/suggest/src/main/webapp/ajax.js
@@ -2,12 +2,16 @@ function sendKeyword(params) {
      return fetch(new Request('suggest.jsp', {
      method: 'post',
      headers: {
-       "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+       "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
      },
-     body: params
+     body: params 
        }))
-       .then(response => response.text());   
+       .then(response => response.text());
 }
+// post방식일 때만 header와 body를 사용한다.
+// get방식의 ?keyword = 자 처럼 params를 옮기지 않고
+// header를 통해서 params를 옮긴다. 그렇기 때문에 headers에서 content-type 명시가 필요하다.
+
 function select(selectedKeyword) {
     document.search.keyword.value = selectedKeyword;
      
