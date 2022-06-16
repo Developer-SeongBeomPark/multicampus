bd9592436c2d77aa7635e4c1dd3da016087c5f69
Developer-SeongBeomPark <tjdqja1570@gmail.com>
2022-06-16 22:26:03 +0900

    TIL

--- code/spring/AI_web_test2/src/main/resources/static/js/test.js
+++ code/spring/AI_web_test2/src/main/resources/static/js/test.js
@@ -1,6 +1,8 @@
 $(function(){
 	$("#login_btn").click(function(){
-
+		let id = document.getElementById("id").value;
+		let pw = document.getElementById("pw").value;
+		alert(id + pw);
 		
 	});
 });
